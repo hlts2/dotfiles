3be3dc03fb4dffbdba074a5c5dd97c467ddca1cc
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-06-07 00:07:23 +0900

    fixed link and type name

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!bin/bash
 
 rm $HOME/.zshrc
-ln zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 rm $HOME/.vimrc
 ln vimrc $HOME/.vimrc
 
 mkdir -p $HOME/.config/nvim/plugged
 rm $HOME/.config/nvim/init.vim
-ln init.vim $HOME/.config/nvim/init.vim
+ln -sf $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 
 rm $HOME/.tmux.conf
-ln tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 
 cp $HOME/.gitignore $HOME/.gitignore_back
 rm $HOME/.gitignore
-ln gitignore $HOME/.gitignore
+ln -sf $HOME/dotfiles/.gitignore $HOME/.gitignore
