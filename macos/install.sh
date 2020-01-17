51b112dbc93a2ef363bb9a6344bfe7a161e9197f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-01-17 09:45:21 +0900

    fix

--- macos/install.sh
+++ macos/install.sh
@@ -13,6 +13,10 @@ brew upgrade --all
 brew cleanup
 brew bundle
 
+# Because did not install with the brew bundle
+brew tap caskroom/fonts
+brew cask install font-fira-code
+
 gem update --system
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
