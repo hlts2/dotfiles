435d31d613c4d24083dcfee10b51555cd2dae74b
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-03 13:30:07 +0900

    fix

--- macos/install.sh
+++ macos/install.sh
@@ -24,31 +24,37 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # -- For Docker
+HANDOLINT_VERSION=1.15.0
+KUBECTL_VERSION=1.17.0
+STERN_VERSION=1.11.0
+K9S_VERSION=0.13.6
+KIND_VERSION=0.7.0
+
 ## hadolint
-curl -L -O https://github.com/hadolint/hadolint/releases/download/v1.15.0/hadolint-$(uname)-x86_64
+curl -L -O https://github.com/hadolint/hadolint/releases/download/v${HANDOLINT_VERSION}/hadolint-Darwin-x86_64
 mv hadolint-$(uname)-x86_64 hadolint
 chmod +x hadolint
-sudo mv ./hadolint /usr/local/bin/hadolint
+#sudo mv ./hadolint /usr/local/bin/hadolint
 
 # -- For Kubernetes --
 ## kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/darwin/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/darwin/amd64/kubectl
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+#sudo mv ./kubectl /usr/local/bin/kubectl
 
 ## stern
-curl -Lo stern https://github.com/wercker/stern/releases/download/1.11.0/stern_$(uname)_amd64
+curl -Lo stern https://github.com/wercker/stern/releases/download/${STERN_VERSION}/stern_Darwin_amd64
 chmod +x stern
-mv ./stern /usr/local/bin/stern
+#mv ./stern /usr/local/bin/stern
 
 ## k9s
-wget https://github.com/derailed/k9s/releases/download/v0.13.6/k9s_0.13.6_$(uname)_x86_64.tar.gz
+wget https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_${K9S_VERSION}_Darwin_x86_64.tar.gz
 tar -xvf k9s_0.13.6_Darwin_x86_64.tar.gz
 rm -rf k9s_0.13.6_Darwin_x86_64.tar.gz
 chmod +x k9s
-mv ./k9s /usr/local/bin/k9s
+#mv ./k9s /usr/local/bin/k9s
 
 ## kind
-curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64"
+curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/kind-Darwin-amd64"
 chmod +x kind
-mv ./kind /usr/local/kind
+#mv ./kind /usr/local/kind
