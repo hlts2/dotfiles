a8442b3811682402ee8c1de9be3e539c85c74984
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-04-07 01:50:27 +0900

    feat: install all package
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- linux/arch/install.sh
+++ linux/arch/install.sh
@@ -115,7 +115,7 @@ swapon ${SWAP_PART} && sync
 # --------------------------
 echo start to download deps
 reflector --country Japan --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap ${ROOT} base linux linux-firmware iwctl
+pacstrap -i ${ROOT} - < pkg.list
 
 # --------------------------
 # ---- fstab ---------------
