46528b1ec8bce3322c3e6227d2317e2485dd2acb
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-05-26 22:10:02 +0900

    fix

--- linux/ubuntu/install.sh
+++ linux/ubuntu/install.sh
@@ -73,3 +73,28 @@ npm install -g \
 
 wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+
+
+# Install deps for k8s
+
+## kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+## kubectx && kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+## kustomize
+curl -s "https://raw.githubusercontent.com/\
+kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo mv ./kustomize /usr/local/kustomize
+
+## helmfile
+wget https://github.com/roboll/helmfile/releases/download/v0.139.7/helmfile_linux_amd64
+chmod +x helmfile_linux_amd64 && sudo mv helmfile_linux_amd64 /usr/local/bin/helmfile
+
+## k9s
+curl -sS https://webinstall.dev/k9s | bash
