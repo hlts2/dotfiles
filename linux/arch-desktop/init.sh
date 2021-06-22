dd58ddd32d9bb18e606194450d74edef27f9539f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-06-23 02:06:03 +0900

    fix
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- linux/arch-desktop/init.sh
+++ linux/arch-desktop/init.sh
@@ -2,10 +2,5 @@
 
 WHOAMI=`whoami`
 
-echo $WHOAMI
-
-sudo chown -R $WHOAMI:$WHOAMI /usr/bin
 sudo chown -R $WHOAMI:$WHOAMI /usr/lib/node_modules
-
-sudo chown -R $WHOAMI:$WHOAMI $HOME
-sudo chmod 755 $HOME
+sudo chown -R $WHOAMI:$WHOAMI /usr/bin/npm
