78406644b0f6a82126d8f283c222f16b63ac7367
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2022-04-08 01:13:27 +0900

    fix
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- linux/arch-desktop/init.sh
+++ linux/arch-desktop/init.sh
@@ -4,3 +4,8 @@ WHOAMI=`whoami`
 
 sudo chown -R $WHOAMI:$WHOAMI /usr/lib/node_modules
 sudo chown -R $WHOAMI:$WHOAMI /usr/bin/npm
+
+
+
+sudo gpasswd -a $USER vboxusers
+sudo sh -c "echo modprobe vboxdrv > /etc/modules-load.d/vbox.conf"
