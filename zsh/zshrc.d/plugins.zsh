f995295d671a2b5882f9a7cdbb5d2ff34e1e9820
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-07-07 17:05:21 +0900

    tweak
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- zsh/zshrc.d/plugins.zsh
+++ zsh/zshrc.d/plugins.zsh
@@ -13,17 +13,11 @@ zplug "zsh-users/zsh-completions", as:plugin, use:"src"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search"
 zplug "romkatv/powerlevel10k", as:theme, depth:1
-# zplug "junegunn/fzf", from:gh-r
-# zplug "junegunn/fzf", from:gh-r, as:command, rename-to:fzf
 
 if ! zplug check --verbose; then
     zplug install
 fi
 
 zplug load
-
-# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-# fi
 
 source $HOME/.p10k.zsh
