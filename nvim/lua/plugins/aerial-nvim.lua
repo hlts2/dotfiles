dd463671aea893ddc0b42c3459e091b4232faa62
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-07-10 00:27:34 +0900

    fix aerial layout
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- nvim/lua/plugins/aerial-nvim.lua
+++ nvim/lua/plugins/aerial-nvim.lua
@@ -3,10 +3,12 @@ local M = {}
 M.default_options = function ()
     return {
         layout = {
-            max_width = { 100, 0.4 },
+            max_width = { 100, 0.5 },
             width = nil,
             min_width = 10,
             default_direction = "prefer_right",
+            -- default_direction = "float",
+            placement = "window",
         },
     }
 end
