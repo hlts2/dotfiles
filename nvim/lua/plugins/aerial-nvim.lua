096acece5cb3697f74ecb04d5817e6ea40a0bf5f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-07-28 14:13:56 +0900

    setup floating window for aerial nvim
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- nvim/lua/plugins/aerial-nvim.lua
+++ nvim/lua/plugins/aerial-nvim.lua
@@ -6,9 +6,22 @@ M.default_options = function ()
             max_width = { 100, 0.5 },
             width = nil,
             min_width = 10,
-            default_direction = "prefer_right",
-            -- default_direction = "float",
+            -- default_direction = "prefer_right",
+            default_direction = "float",
             placement = "window",
+            attach_mode = "global",
+            -- placement = "edge",
+        },
+
+        autojump = true,
+        highlight_on_hover = true,
+        close_on_select = true,
+
+        float = {
+            relative = "editor",
+            max_width = { 100, 0.5 },
+            width = nil,
+            min_width = 10,
         },
     }
 end
