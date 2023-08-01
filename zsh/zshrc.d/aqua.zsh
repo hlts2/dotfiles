f7c425f7784d96facf30dc07380b845c5ff3f253
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-08-02 00:33:03 +0900

    add cli package using aqua
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- zsh/zshrc.d/aqua.zsh
+++ zsh/zshrc.d/aqua.zsh
@@ -1,9 +1,7 @@
 if ! type aqua > /dev/null 2>&1; then
-    curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
-    echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
-    chmod +x aqua-installer
-    ./aqua-installer
-    rm -rf aqua-installer
+    if type go > /dev/null 2>&1; then
+        go install github.com/aquaproj/aqua/v2/cmd/aqua@v2.11.0-4
+    fi
 fi
 
-aqua install -a
+# aqua install -a
