b4455cfb7c3fd0a0ce046937051351db9d33f714
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-08-02 17:00:08 +0900

    deleted unnecessary check
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -2,9 +2,7 @@ if [ ! -d $HOME/.fzf ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 fi
 
-if [ -f $HOME/.fzf.zsh ]; then
-    if type fzf > /dev/null 2>&1; then
-        source $HOME/.fzf/shell/completion.zsh
-        source $HOME/.fzf/shell/key-bindings.zsh
-    fi
+if type fzf > /dev/null 2>&1; then
+    source $HOME/.fzf/shell/completion.zsh
+    source $HOME/.fzf/shell/key-bindings.zsh
 fi
