719bb403e6f883719696ab9fc535c5e937685603
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-08-21 11:06:28 +0900

    add modes.nvim plugin
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -27,12 +27,20 @@ local default_plugins = {
 
     {
         'romgrk/barbar.nvim',
+        version = '^1.0.0',                -- optional: only update when a new 1.x version is released
         dependencies = {
             'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
             'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
         },
         -- init = function() vim.g.barbar_auto_setup = false end,
-        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
+
+    {
+        'mvllow/modes.nvim',
+        tag = 'v0.2.0',
+        opts = function()
+            return require('plugins/modes-nvim').default_options()
+        end,
     },
 
     --------------------------------
