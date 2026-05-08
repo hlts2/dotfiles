6888d71a74fed4509c976f68ccdc652eea63aedb
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2026-05-08 13:13:03 +0900

    feat(nvim): lazy-load go plugins on go ft
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- nvim/lua/plugins/lang/go.lua
+++ nvim/lua/plugins/lang/go.lua
@@ -1,5 +1,5 @@
 return {
-	{ "mattn/vim-goimports" },
-	{ "mattn/vim-goaddtags" },
-	{ "kyoh86/vim-go-coverage" },
+	{ "mattn/vim-goimports", ft = "go" },
+	{ "mattn/vim-goaddtags", ft = "go" },
+	{ "kyoh86/vim-go-coverage", ft = "go" },
 }
