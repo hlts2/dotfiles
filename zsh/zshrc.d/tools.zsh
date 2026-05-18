66f4166fefc1c86b7a0fe451a6b219a96f8b3110
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2026-05-19 03:59:48 +0900

    feat(zsh): load zoxide settings
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- zsh/zshrc.d/tools.zsh
+++ zsh/zshrc.d/tools.zsh
@@ -17,3 +17,13 @@ if (( $+commands[fzf] )); then
     source "$_fzf_cache"
     unset _fzf_cache
 fi
+
+if (( $+commands[zoxide] )); then
+    _zoxide_cache="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zoxide.zsh"
+    if [[ ! -s $_zoxide_cache || $commands[zoxide] -nt $_zoxide_cache ]]; then
+        mkdir -p "${_zoxide_cache:h}"
+        zoxide init zsh > "$_zoxide_cache"
+    fi
+    source "$_zoxide_cache"
+    unset _zoxide_cache
+fi
